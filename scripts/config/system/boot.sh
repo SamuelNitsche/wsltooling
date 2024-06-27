e0958eba49bdc99ea31c143e6c87f1638388ef09
c.herrmann <c.herrmann@wagner-ecommerce.group>
2024-06-27 17:26:26 +0200

    Autom. Anpassung des MySQL Nutzers root@localhost
    Die Updateprozedur des MySQL-Pakets stellt sicher, dass die Standardnutzer die Authentifizierungsmethode "caching_sha2_password" verwenden, da die Methode "mysql_native_password" deprecated ist. Solange wir diese Methode jedoch noch verwenden, sorgt das hinzugefuegte Command dafür, dass sich der Nutzer "root@localhost" weiterhin mit dieser authentifizieren kann.

--- scripts/config/system/boot.sh
+++ scripts/config/system/boot.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Ensure correct dns resolution
 sed -i '/nameserver/d' /etc/resolv.conf
-
 ip route show | grep -i default | awk '{ print "nameserver "$3}' | tee -a /etc/resolv.conf > /dev/null
+
+# Ensure passwordless root authentication for method 'mysql_native_password'
+# (The update procedure of MySQL tends to change the authentication method for default users)
+mysql -uroot -e "ALTER user 'root'@'localhost' IDENTIFIED WITH mysql_native_password by ''"
